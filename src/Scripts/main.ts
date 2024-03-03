75b81728cb80d0b587fa19e62620d9a86c08991e
Ben Spaulding <ben@spaulding.im>
2024-03-02 21:43:12 -0700

    More fussing

--- src/Scripts/main.ts
+++ src/Scripts/main.ts
@@ -1,53 +1,60 @@
 'use strict';
 
-function activate(): void {
-	console.log('Activated!');
-
-	console.group('-> nova');
-	// https://docs.nova.app/api-reference/environment/
-	console.log(nova);
-	console.log(Object.getPrototypeOf(nova));
-	// properties
-	console.log(nova.assistants);
-	console.log(nova.clipboard);
-	console.log(nova.commands);
-	console.log(nova.config);
-	console.log(nova.credentials);
-	console.log(nova.crypto);
-	console.log(nova.environment);
-	console.log(nova.extension);
-	console.log(nova.fs);
-	console.log(nova.locales);
-	console.log(nova.notifications);
-	console.log(nova.path);
-	console.log(nova.subscriptions);
-	console.log(nova.systemVersion);
-	console.log(nova.version);
-	console.log(nova.versionString);
-	console.log(nova.workspace);
-	// methods
-	console.log(nova.beep());
-	console.log(nova.localize('run'));
-	console.log(nova.isReleasedVersion());
-	console.log(nova.inDevMode());
-	console.log(nova.openConfig);
-	console.log(nova.openURL);
-	console.groupEnd();
-
-	// vars
+if (nova.inDevMode()) {
+	console.log();
 	console.group('-> this');
 	console.log(this);
-	for (const name in this) {
+	for (const name in this as unknown as object) {
 		console.log(name);
 	}
 	console.groupEnd();
+	console.log();
+}
+
+function activate(this: object): void {
+	console.log('Activated!');
+
+	if (nova.inDevMode()) {
+		console.log();
+		console.group('-> nova');
+		// https://docs.nova.app/api-reference/environment/
+		console.log(nova);
+		console.log(Object.getPrototypeOf(nova));
+		// properties
+		console.log(nova.assistants);
+		console.log(nova.clipboard);
+		console.log(nova.commands);
+		console.log(nova.config);
+		console.log(nova.credentials);
+		// console.log(nova.crypto); // [TODO] Make PR to add this to nova plugin.
+		console.log(nova.environment);
+		console.log(nova.extension);
+		console.log(nova.fs);
+		console.log(nova.locales);
+		console.log(nova.notifications);
+		console.log(nova.path);
+		console.log(nova.subscriptions);
+		console.log(nova.systemVersion);
+		console.log(nova.version);
+		console.log(nova.versionString);
+		console.log(nova.workspace);
+		// methods
+		console.log(nova.beep());
+		console.log(nova.localize('run'));
+		console.log(nova.isReleasedVersion());
+		console.log(nova.inDevMode());
+		console.log(nova.openConfig);
+		console.log(nova.openURL);
+		console.groupEnd();
+		console.log();
+	}
 }
 
 function deactivate(): void {
 	console.log('Deactivated!');
 }
 
-module.exports = exports = {
+module.exports = {
 	activate,
 	deactivate,
 };
